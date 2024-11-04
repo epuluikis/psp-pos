e3692c0bb4ecb41772f8c55f6e943e8792751fba
Titas Stankevičius <stank.titas@gmail.com>
2024-11-04 14:27:07 +0200

    migration and fixing create and read all endpoints

---
+++ Looms.PoS/Looms.PoS.Application/Mappings/Profiles/BusinessProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Looms.PoS.Application.Models.Requests;
+using Looms.PoS.Application.Models.Responses;
+using Looms.PoS.Domain.Daos;
+
+namespace Looms.PoS.Application.Mappings.Profiles;
+
+public class BusinessProfile : Profile
+{
+    public BusinessProfile()
+    {
+        CreateMap<CreateBusinessRequest, BusinessDao>(MemberList.Source)
+            .ForMember(dest => dest.OwnerName, opt => opt.MapFrom(src => src.Owner));
+
+        CreateMap<BusinessDao, BusinessResponse>()
+            .ForMember(dest => dest.Owner, opt => opt.MapFrom(src => src.OwnerName));
+    }
+}
---
+++ Looms.PoS/Looms.PoS.Persistance/Migrations/20241104120409_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Looms.PoS.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Businesses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    OwnerName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Businesses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Businesses");
+        }
+    }
+}
