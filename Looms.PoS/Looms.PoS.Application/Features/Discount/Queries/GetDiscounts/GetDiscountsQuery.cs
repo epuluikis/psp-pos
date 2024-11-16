6c409407a476a50dc7105108fa3f1360a8211f31
aiste. <98818740+aiste-i@users.noreply.github.com>
2024-11-16 23:58:40 +0200

    GetDiscountsQuery new line after namespace

--- Looms.PoS/Looms.PoS.Application/Features/Discount/Queries/GetDiscounts/GetDiscountsQuery.cs
+++ Looms.PoS/Looms.PoS.Application/Features/Discount/Queries/GetDiscounts/GetDiscountsQuery.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Looms.PoS.Application.Features.Discount.Queries;
+
 public record GetDiscountsQuery : LoomsHttpRequest, IRequest<IActionResult>
 {
     public GetDiscountsQuery(HttpRequest request) : base(request)
