5884e6d9175ab965a6c86f08248ce097b7599aa4
Matas Švalrys <115905205+MatasSvarlys@users.noreply.github.com>
2024-12-16 22:44:43 +0200

    added migrations

---
+++ Looms.PoS/Looms.PoS.Persistance/Migrations/20241216203129_AddProductsAndProductVariations.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Looms.PoS.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductsAndProductVariations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "PaymentTerminals",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "PaymentProviders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EndHour",
+                table: "Businesses",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StartHour",
+                table: "Businesses",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    DurationMin = table.Column<int>(type: "integer", nullable: false),
+                    BusinessId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TaxId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Services_Businesses_BusinessId",
+                        column: x => x.BusinessId,
+                        principalTable: "Businesses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Services_Taxes_TaxId",
+                        column: x => x.TaxId,
+                        principalTable: "Taxes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ServiceId = table.Column<Guid>(type: "uuid", nullable: false),
+                    EmployeeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CustomerName = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    AppointmentTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    Comment = table.Column<string>(type: "text", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Services_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Services",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Users_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductVariations_ProductId",
+                table: "ProductVariations",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_BusinessId",
+                table: "Products",
+                column: "BusinessId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_TaxId",
+                table: "Products",
+                column: "TaxId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_EmployeeId",
+                table: "Reservations",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_ServiceId",
+                table: "Reservations",
+                column: "ServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_BusinessId",
+                table: "Services",
+                column: "BusinessId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_TaxId",
+                table: "Services",
+                column: "TaxId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Businesses_BusinessId",
+                table: "Products",
+                column: "BusinessId",
+                principalTable: "Businesses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Taxes_TaxId",
+                table: "Products",
+                column: "TaxId",
+                principalTable: "Taxes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductVariations_Products_ProductId",
+                table: "ProductVariations",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Businesses_BusinessId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Taxes_TaxId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductVariations_Products_ProductId",
+                table: "ProductVariations");
+
+            migrationBuilder.DropTable(
+                name: "Reservations");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductVariations_ProductId",
+                table: "ProductVariations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_BusinessId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_TaxId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "PaymentTerminals");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "PaymentProviders");
+
+            migrationBuilder.DropColumn(
+                name: "EndHour",
+                table: "Businesses");
+
+            migrationBuilder.DropColumn(
+                name: "StartHour",
+                table: "Businesses");
+        }
+    }
+}
